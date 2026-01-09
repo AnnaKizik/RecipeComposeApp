04c42f371383da98940193335168715f705013b1
anna_kizik <a.v.kizik@gmail.com>
2026-01-09 15:39:09 +0300

    RCA-14

--- app/src/main/java/com/yourcompany/recipecomposeapp/ui/categories/CategoriesScreen.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/ui/categories/CategoriesScreen.kt
@@ -1,26 +1,25 @@
 package com.yourcompany.recipecomposeapp.ui.categories
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.yourcompany.recipecomposeapp.R
 import com.yourcompany.recipecomposeapp.core.ui.ScreenHeader
+import com.yourcompany.recipecomposeapp.data.repository.RecipesRepositoryStub
+import com.yourcompany.recipecomposeapp.ui.categories.model.toUiModel
 import com.yourcompany.recipecomposeapp.ui.theme.RecipeComposeAppTheme
 
 @Composable
@@ -35,7 +34,10 @@ fun CategoriesScreen(
                     "КАТЕГОРИИ",
                     R.drawable.bcg_categories
                 )
-                CategoriesList()
+                CategoriesList(
+                    onCategoryClick = {categoryId ->
+                        RecipesRepositoryStub.getRecipesByCategoryId(categoryId)}
+                )
             }
         }
     )
@@ -43,46 +45,56 @@ fun CategoriesScreen(
 
 @Composable
 fun CategoriesList(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onCategoryClick: (categoryId: Int) -> Unit,
 ) {
+    var categoriesList by remember {
+        mutableStateOf(RecipesRepositoryStub.takeCategories())
+    }
+    val categoriesUiModelList = categoriesList.map { it.toUiModel() }
     LazyVerticalGrid(
         modifier = modifier.fillMaxSize(),
         columns = GridCells.Fixed(2),
         verticalArrangement = Arrangement.spacedBy(8.dp),
         horizontalArrangement = Arrangement.spacedBy(4.dp)
     ) {
-        items(6) {
-            Surface(
-                modifier = modifier
-                    .padding(16.dp),
-                shape = RoundedCornerShape(8.dp),
-                color = MaterialTheme.colorScheme.surface,
-            ) {
-                Column {
-                    Image(
-                        modifier = modifier
-                            .fillMaxSize()
-                            .height(130.dp),
-                        painter = painterResource(R.drawable.stub),
-                        contentDescription = null,
-                        contentScale = ContentScale.Crop
-                    )
-                    Text(
-                        text = "Название",
-                        modifier = modifier.padding(8.dp),
-                        color = MaterialTheme.colorScheme.primary,
-                        style = MaterialTheme.typography.titleMedium
-                    )
-                    Text(
-                        text = "Описание категории",
-                        modifier = modifier
-                            .padding(horizontal = 8.dp)
-                            .padding(bottom = 10.dp),
-                        color = MaterialTheme.colorScheme.secondary,
-                        style = MaterialTheme.typography.bodySmall
-                    )
-                }
-            }
+        items(categoriesUiModelList) {category ->
+            CategoryItem(
+                imageUrl = category.imageUrl,
+                title = category.title,
+                description = category.description
+            ){}
+//            Surface(
+//                modifier = modifier
+//                    .padding(16.dp),
+//                shape = RoundedCornerShape(8.dp),
+//                color = MaterialTheme.colorScheme.surface,
+//            ) {
+//                Column {
+//                    Image(
+//                        modifier = modifier
+//                            .fillMaxSize()
+//                            .height(130.dp),
+//                        painter = painterResource(R.drawable.stub),
+//                        contentDescription = null,
+//                        contentScale = ContentScale.Crop
+//                    )
+//                    Text(
+//                        text = "Название",
+//                        modifier = modifier.padding(8.dp),
+//                        color = MaterialTheme.colorScheme.primary,
+//                        style = MaterialTheme.typography.titleMedium
+//                    )
+//                    Text(
+//                        text = "Описание категории",
+//                        modifier = modifier
+//                            .padding(horizontal = 8.dp)
+//                            .padding(bottom = 10.dp),
+//                        color = MaterialTheme.colorScheme.secondary,
+//                        style = MaterialTheme.typography.bodySmall
+//                    )
+//                }
+//            }
         }
     }
 }
---
+++ app/src/main/java/com/yourcompany/recipecomposeapp/ui/categories/CategoryItem.kt
@@ -0,0 +1,73 @@
+package com.yourcompany.recipecomposeapp.ui.categories
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import com.yourcompany.recipecomposeapp.R
+import com.yourcompany.recipecomposeapp.ui.theme.RecipeComposeAppTheme
+
+@Composable
+fun CategoryItem(
+    modifier: Modifier = Modifier,
+    imageUrl: String,
+    title: String,
+    description: String,
+    onClick: () -> Unit,
+) {
+    Card(
+        onClick = onClick,
+        modifier = modifier,
+    ) {
+        Column(
+            modifier = modifier.background(color = MaterialTheme.colorScheme.surface)
+        ) {
+            AsyncImage(
+                modifier = modifier
+                    .height(130.dp),
+                model = imageUrl,
+                placeholder = painterResource(R.drawable.img_placeholder),
+                error = painterResource(R.drawable.img_error),
+                contentDescription = "Изображение категории",
+            )
+            Text(
+                text = title,
+                modifier = modifier.padding(8.dp),
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = description,
+                modifier = modifier
+                    .padding(horizontal = 8.dp)
+                    .padding(bottom = 10.dp),
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.bodySmall
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryItemPreview() {
+    RecipeComposeAppTheme {
+        CategoryItem(
+            imageUrl = "burger.png",
+            title = "ЗАГОЛОВОК",
+            description = "Описание категории",
+            onClick = {},
+        )
+    }
+}
