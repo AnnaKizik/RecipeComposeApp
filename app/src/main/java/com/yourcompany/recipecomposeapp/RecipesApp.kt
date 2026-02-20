a1870be6f35878b32838791e9c381585078c19d3
a.kizik <a.v.kizik@gmail.com>
2026-02-20 11:52:22 +0300

    RCA-21

--- app/src/main/java/com/yourcompany/recipecomposeapp/RecipesApp.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/RecipesApp.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -18,11 +19,13 @@ import com.yourcompany.recipecomposeapp.ui.details.RecipeDetailsScreen
 import com.yourcompany.recipecomposeapp.ui.favorites.FavoritesScreen
 import com.yourcompany.recipecomposeapp.ui.recipes.RecipesScreen
 import com.yourcompany.recipecomposeapp.ui.theme.RecipeComposeAppTheme
+import com.yourcompany.recipecomposeapp.util.FavoritePrefsManager
 import kotlinx.coroutines.delay
 
 @Composable
 fun RecipesApp(deepLinkIntent: Intent?) {
     val navController = rememberNavController()
+    val context = LocalContext.current
 
     LaunchedEffect(deepLinkIntent) {
         deepLinkIntent?.data?.let { uri ->
@@ -87,7 +90,17 @@ fun RecipesApp(deepLinkIntent: Intent?) {
                 ) { backStackEntry ->
                     val recipeId = backStackEntry.arguments?.getInt("recipeId") ?: 0
                     val recipe = RecipesRepositoryStub.getRecipeDataByRecipeId(recipeId)
-                    RecipeDetailsScreen(recipe = recipe)
+                    RecipeDetailsScreen(
+                        recipe = recipe,
+                        isFavorite = FavoritePrefsManager(context).checkIsFavorite(recipeId),
+                        onToggleFavorite = {
+                            if (recipe.isFavorite) {
+                                FavoritePrefsManager(context).removeFromFavorite(recipe.id)
+                            } else {
+                                FavoritePrefsManager(context).addToFavorites(recipe.id)
+                            }
+                        },
+                    )
                 }
                 composable(
                     route = Destination.Favorites.route,
@@ -105,6 +118,6 @@ fun RecipesApp(deepLinkIntent: Intent?) {
 
 @Preview(showBackground = true)
 @Composable
-fun RecipesAppPreview() {
+private fun RecipesAppPreview() {
     RecipesApp(null)
 }
