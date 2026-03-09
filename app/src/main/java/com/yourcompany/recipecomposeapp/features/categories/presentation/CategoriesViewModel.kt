411ca8c43df7c6299c90868ddbbac8fda80dff82
avkiz <a.v.kizik@gmail.com>
2026-03-09 10:38:17 +0300

    RCA-26

---
+++ app/src/main/java/com/yourcompany/recipecomposeapp/features/categories/presentation/CategoriesViewModel.kt
@@ -0,0 +1,40 @@
+package com.yourcompany.recipecomposeapp.features.categories.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.yourcompany.recipecomposeapp.data.repository.RecipesRepositoryStub
+import com.yourcompany.recipecomposeapp.features.categories.presentation.model.CategoriesUiState
+import com.yourcompany.recipecomposeapp.features.categories.presentation.model.toUiModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class CategoriesViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(CategoriesUiState())
+    val uiState: StateFlow<CategoriesUiState> = _uiState.asStateFlow()
+
+    init {
+        loadCategories()
+    }
+
+    private fun loadCategories() {
+        viewModelScope.launch {
+            try {
+                _uiState.value = CategoriesUiState(
+                    categories = RecipesRepositoryStub.takeCategories().map { it.toUiModel() },
+                    isLoaded = true,
+                    error = null,
+                )
+            } catch (e: Exception) {
+                _uiState.update { currentState ->
+                    currentState.copy(
+                        error = "Возникла ошибка при загрузке категорий: $e"
+                    )
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/yourcompany/recipecomposeapp/features/categories/ui/CategoriesScreen.kt
+++ app/src/main/java/com/yourcompany/recipecomposeapp/features/categories/ui/CategoriesScreen.kt
@@ -7,27 +7,32 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.yourcompany.recipecomposeapp.R
 import com.yourcompany.recipecomposeapp.core.ui.ImageResource
 import com.yourcompany.recipecomposeapp.core.ui.ScreenHeader
-import com.yourcompany.recipecomposeapp.data.repository.RecipesRepositoryStub
-import com.yourcompany.recipecomposeapp.features.categories.presentation.model.toUiModel
 import com.yourcompany.recipecomposeapp.core.ui.theme.RecipeComposeAppTheme
+import com.yourcompany.recipecomposeapp.features.categories.presentation.CategoriesViewModel
+import com.yourcompany.recipecomposeapp.features.categories.presentation.model.CategoryUiModel
 
 @Composable
 fun CategoriesScreen(
     modifier: Modifier = Modifier,
     onCategoryClick: (Int) -> Unit,
 ) {
+    val viewModel: CategoriesViewModel = viewModel()
+    val uiState by viewModel.uiState.collectAsState()
+
     Scaffold(
         modifier = modifier,
         content = { paddingValues ->
@@ -36,11 +41,21 @@ fun CategoriesScreen(
                     "КАТЕГОРИИ",
                     ImageResource.ResourceId(R.drawable.bcg_categories)
                 )
-                CategoriesList(
-                    onCategoryClick = { categoryId ->
-                        onCategoryClick(categoryId)
-                    }
-                )
+                if (uiState.isLoaded) {
+                    CategoriesList(
+                        categoriesList = uiState.categories,
+                        onCategoryClick = { categoryId ->
+                            onCategoryClick(categoryId)
+                        }
+                    )
+                } else {
+                    Text(
+                        text = uiState.error ?: "Возникла ошибка при загрузке категорий",
+                        color = MaterialTheme.colorScheme.secondary,
+                        style = MaterialTheme.typography.labelLarge,
+                        textAlign = TextAlign.Center,
+                    )
+                }
             }
         }
     )
@@ -48,13 +63,10 @@ fun CategoriesScreen(
 
 @Composable
 fun CategoriesList(
+    categoriesList: List<CategoryUiModel>,
     modifier: Modifier = Modifier,
     onCategoryClick: (Int) -> Unit,
 ) {
-    var categoriesList by remember {
-        mutableStateOf(RecipesRepositoryStub.takeCategories())
-    }
-    val categoriesUiModelList = categoriesList.map { it.toUiModel() }
     LazyVerticalGrid(
         modifier = modifier
             .fillMaxSize()
@@ -64,7 +76,7 @@ fun CategoriesList(
         horizontalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         items(
-            categoriesUiModelList,
+            categoriesList,
             key = { it.id }) { category ->
             CategoryItem(
                 imageUrl = category.imageUrl,
