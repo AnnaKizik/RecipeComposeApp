bb684d3ef688583b9c8e717391dc6daacf9cdec6
a.kizik <a.v.kizik@gmail.com>
2026-04-08 10:09:32 +0300

    RCA-39

---
+++ app/src/main/java/com/yourcompany/recipecomposeapp/data/database/converter/Converters.kt
@@ -0,0 +1,16 @@
+package com.yourcompany.recipecomposeapp.data.database.converter
+
+import androidx.room.TypeConverter
+
+class Converters {
+
+    @TypeConverter
+    fun fromString(value: String): List<String> {
+        return value.split("|||")
+    }
+
+    @TypeConverter
+    fun fromList(list: List<String>): String {
+        return list.joinToString("|||")
+    }
+}
